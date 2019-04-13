21846c70535ee6cdb47375579c83d60855f228a3
Nick Anderson <nickanderson@gmail.com>
2019-04-12 17:39:49 -0700

    unique keys

--- src/Components/Scatterplot.js
+++ src/Components/Scatterplot.js
@@ -2,13 +2,20 @@ import React, { Component } from "react";
 import * as d3 from "d3";
 
 class Scatterplot extends Component {
+  dataPlotHandler = data => {
+    return data.map(([x, y]) => (
+      <circle cx={x} cy={y} r="3" key={`${x + y}`} />
+    ));
+  };
+
   render() {
     const { x, y, data } = this.props;
     return (
       <g transform={`translate(${x}, ${y})`}>
-        {data.map(([x, y]) => (
+        {/* {data.map(([x, y]) => (
           <circle cx={x} cy={y} r="3" />
-        ))}
+        ))} */}
+        {this.dataPlotHandler(data)}
       </g>
     );
   }
