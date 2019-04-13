7e3dfd79669d7f1dec8f44d907de5f4f501614e7
Nick Anderson <nickanderson@gmail.com>
2019-04-12 17:14:51 -0700

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+# React D3 Basic scatter plot graph
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as d3 from "d3";
 
 import "./styles.css";
 
+const data = d3.range(100).map(() => [Math.random(), Math.random()]);
+
+console.log(data);
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Basic D3 Scatter Plot Graph</h1>
     </div>
   );
 }
