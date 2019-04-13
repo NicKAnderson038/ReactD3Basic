3b22b861cdd05635502dfcec14ce6e726ce9b916
Nick Anderson <nickanderson@gmail.com>
2019-04-12 17:44:13 -0700

    scaling

--- src/Components/Scatterplot.js
+++ src/Components/Scatterplot.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import * as d3 from "d3";
 
 class Scatterplot extends Component {
+  xScale = d3
+    .scaleLinear()
+    .domain([0, 1])
+    .range([0, 300]);
+  yScale = d3
+    .scaleLinear()
+    .domain([0, 1])
+    .range([0, 200]);
+
   dataPlotHandler = data => {
     return data.map(([x, y]) => (
       <circle cx={x} cy={y} r="3" key={`${x + y}`} />
@@ -10,8 +19,12 @@ class Scatterplot extends Component {
 
   render() {
     const { x, y, data } = this.props;
+    const xScale = this.xScale(x);
+    const yScale = this.yScale(y);
     return (
-      <g transform={`translate(${x}, ${y})`}>{this.dataPlotHandler(data)}</g>
+      <g transform={`translate(${xScale}, ${yScale})`}>
+        {this.dataPlotHandler(data)}
+      </g>
     );
   }
 }
