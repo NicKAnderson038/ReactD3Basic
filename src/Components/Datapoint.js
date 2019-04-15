41d7b6583c205c4a85a1d458cc87154b42d19aaa
Nick Anderson <nickanderson@gmail.com>
2019-04-15 13:43:55 -0700

    styled points

--- src/Components/Datapoint.js
+++ src/Components/Datapoint.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import styled from '@emotion/styled'
 
-class Datapoint extends Component {
-  render() {
-    const { x, y } = this.props
-    return <circle cx={x} cy={y} r="3" />
-  }
+const Circle = styled.circle`
+  fill: steelblue;
+  fill-opacity: 0.8;
+  stroke: steelblue;
+  stroke-width: 1.5px;
+`
+
+// class Datapoint extends Component {
+//   render() {
+//     const { x, y } = this.props
+//     return <circle cx={x} cy={y} r="3" />
+//   }
+// }
+
+// export default Datapoint
+
+const datapoint = props => {
+  const { x, y } = props
+  return <Circle cx={x} cy={y} r="3" />
 }
 
-export default Datapoint
+export default datapoint
