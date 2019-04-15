79da9856a89f1b8347df353685a8b251ccb24cf9
Nick Anderson <nickanderson@gmail.com>
2019-04-15 14:29:12 -0700

    double graphs independent clicks

--- src/index.js
+++ src/index.js
@@ -11,15 +11,33 @@ const data = d3.range(100).map(() => [Math.random(), Math.random()])
 console.log(data)
 class App extends Component {
   state = {
-    width: 300,
-    height: 300
+    width1: 300,
+    height1: 300,
+    width2: 300,
+    height2: 300
   }
 
-  onClick() {
-    this.setState({
-      width: this.state.width * 0.8,
-      height: this.state.height * 0.8
-    })
+  onClick(flag) {
+    switch (flag) {
+      case 'one':
+        this.setState({
+          width1: this.state.width1 * 0.8,
+          height1: this.state.height1 * 0.8
+        })
+        return
+      case 'two':
+        this.setState({
+          width2: this.state.width2 * 0.8,
+          height2: this.state.height2 * 0.8
+        })
+        return
+      default:
+        return null
+    }
+    // this.setState({
+    //   width: this.state.width * 0.8,
+    //   height: this.state.height * 0.8
+    // })
   }
 
   render() {
@@ -27,13 +45,23 @@ class App extends Component {
       <div className="App">
         <h1>Basic D3 Scatter Plot Graph</h1>
         <br />
-        <svg width="800" height="800" onClick={() => this.onClick()}>
+        <svg width="400" height="400" onClick={() => this.onClick('one')}>
           <Scatterplot
             x={50}
             y={50}
             data={data}
-            width={this.state.width}
-            height={this.state.height}
+            width={this.state.width1}
+            height={this.state.height1}
+            datapoint={({ x, y }) => <Datapoint x={x} y={y} />}
+          />
+        </svg>
+        <svg width="400" height="400" onClick={() => this.onClick('two')}>
+          <Scatterplot
+            x={50}
+            y={50}
+            data={data}
+            width={this.state.width2}
+            height={this.state.height2}
             datapoint={({ x, y }) => <Datapoint x={x} y={y} />}
           />
         </svg>
