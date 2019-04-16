25c02ebd4948a17090ab995918861d9d61ac0bde
Nick Anderson <nickanderson@gmail.com>
2019-04-16 07:58:58 -0700

    .

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Basic D3 Scatter Plot Graph</h1>
+        <h1>D3 Scatter Plot Graph</h1>
         <br />
         <svg
           width="400"
